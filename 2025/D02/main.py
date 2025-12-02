fd187109bcd98a6249615ee3a1bf1ae73b29db3b
tbk81 <dev@tbk81.com>
2025-12-02 04:59:21 -0800

    day 2

---
+++ 2025/D02/main.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------ DATA PARSING ----------------------------------------------- #
+with open("test_data.txt") as f:
+    data = f.readlines()
+    data_li = [line.split(",") for line in data]
+# print(data_li)
+
+inner_list = data_li[0]
+parse_li = [[int(x) for x in s.split('-')] for s in inner_list]
+# print(parse_li)
+
+# -------------------------------------------------- PART 1 -------------------------------------------------- #
+for i in range(len(parse_li)):
+    for n in range(parse_li[i][0], parse_li[i][1]+1):
+        print(n)
+    print("\n")
+
+
