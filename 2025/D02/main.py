f6b4ed5725f114c47403e9b59b5213e8bc59fdc1
MBP <dev@tbk81.com>
2026-04-09 17:52:40 -0700

    day8

--- 2025/D02/main.py
+++ 2025/D02/main.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------ DATA PARSING ----------------------------------------------- #
-with open("test_data_2.txt") as f:
+with open("test_data.txt") as f:
     data = f.readlines()
     data_li = [line.split(",") for line in data]
 # print(data_li)
@@ -9,22 +9,15 @@ parse_li = [[int(x) for x in s.split('-')] for s in inner_list]
 # print(parse_li)
 
 # -------------------------------------------------- PART 1 -------------------------------------------------- #
-# for i in range(len(parse_li)):
-#     for n in range(parse_li[i][0], parse_li[i][1]+1):
-#         for digit in str(n):
-#
-#             print(int(digit))
-#         print("\n")
-#     print("\n")
 
-for i in parse_li:
-    first_num = i[0]
-    last_num = i[1] - 1
+def li_maker(li):
+    first_num = li[0]
+    last_num = li[1] - 1
     num = first_num
-    num_li = []
     while num < last_num:
         num += 1
-        i.insert(len(i)-1, num)
-    print(i)
+        li.insert(len(li)-1, num)
+    return li
 
 
+print(li_maker(parse_li[0]))
