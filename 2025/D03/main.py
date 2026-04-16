9464ca7002018f652adb79fc66ea19fbb976014e
MBP <dev@tbk81.com>
2026-04-16 15:38:20 -0700

    day3

--- 2025/D03/main.py
+++ 2025/D03/main.py
@@ -1,7 +1,7 @@
 with open("test_data.txt") as f:
     data = f.readlines()
     data_li = [line.strip() for line in data]
-print(data_li)
+# print(data_li)
 
 # -------------------------------------------------- PART 1 -------------------------------------------------- #
 
@@ -20,8 +20,18 @@ print(data_li)
 
 num = '811111111111119'
 first_num = 0
+first_pos = 0
 second_num = 0
-for n in num:
-    if int(n) > first_num:
-        first_num = int(n)
-print(first_num)
+second_pos = 0
+num_li = list(map(int, num))  # creates a list of int from a str
+print(num_li)
+for i in range(len(num_li)):
+    if num_li[i] > first_num:
+        first_num = num_li[i]
+        first_pos = i
+    # print(i)
+print(first_num, first_pos)
+
+# for n in num:
+#     if int(n) > first_num:
+#         first_num = int(n)
