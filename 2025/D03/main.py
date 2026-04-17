36a396991058ab534abfbaf0c38a767bc2344276
MBP <dev@tbk81.com>
2026-04-16 17:42:59 -0700

    day3

--- 2025/D03/main.py
+++ 2025/D03/main.py
@@ -1,22 +1,23 @@
 import heapq
+from itertools import combinations
 
-with open("test_data.txt") as f:
+with open("data.txt") as f:
     data = f.readlines()
     data_li = [line.strip() for line in data]
 print(data_li)
 
 # -------------------------------------------------- PART 1 -------------------------------------------------- #
 
-def joltage(num):
-    num_li = list(map(int, num))
-    top = heapq.nlargest(2, num_li)
-    pos1 = num_li.index(top[0])
-    pos2 = num_li.index(top[1])
-    if pos1 < pos2:
-        fin_num = int(str(top[0]) + str(top[1]))
-    else:
-        fin_num = int(str(top[1]) + str(top[0]))
-    return fin_num
+# def joltage(num):
+#     num_li = list(map(int, num))
+#     top = heapq.nlargest(2, num_li)
+#     pos1 = num_li.index(top[0])
+#     pos2 = num_li.index(top[1])
+#     if pos1 < pos2:
+#         fin_num = int(str(top[0]) + str(top[1]))
+#     else:
+#         fin_num = int(str(top[1]) + str(top[0]))
+#     return fin_num
 
 
 # joltage_li = []
@@ -25,9 +26,19 @@ def joltage(num):
 # print(joltage_li)
 # print(joltage(test_num))
 
-test_num = '818181911112111'
-num_li = list(map(int, test_num))
-num = 0
-for i in range(len(num_li)):
-    run_num = int(str(num_li[i]) + str(num_li[i+1]))
-    print(run_num)
+def find_joltage(num):
+    num_li = list(map(int, num))
+    all_combos = [int(f"{a}{b}") for a, b in combinations(num_li, 2)]
+    return max(all_combos)
+
+
+joltage_li = []
+for i in data_li:
+    joltage_li.append(find_joltage(i))
+print(joltage_li)
+print(sum(joltage_li))
+
+# -------------------------------------------------- PART 2 -------------------------------------------------- #
+
+
+
