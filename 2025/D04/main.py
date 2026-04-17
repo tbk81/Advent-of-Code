1b38198023a69aab2d47ddaa84274efc9732dd49
MBP <dev@tbk81.com>
2026-04-17 14:35:23 -0700

    day4

--- 2025/D04/main.py
+++ 2025/D04/main.py
@@ -1,17 +1,28 @@
 import numpy as np
 
 
-# def txt_parse(txt):
-#     with open(txt) as f:
-#         data = f.readlines()
-#         return [l.strip() for l in data]
-
 def txt_arr_parse(txt):
     with open(txt) as f:
-        data = f.readlines()
-        return [l.strip() for l in data]
+        return [l.strip().split(' ') for l in f.readlines()]
 
 
 data_arr = txt_arr_parse("test_data.txt")
 print(data_arr)
 
+# -------------------------------------------------- PART 1 -------------------------------------------------- #
+
+
+
+# -------------------------------------------------- PART 2 -------------------------------------------------- #
+
+
+
+# -------------------------------------------------- TESTING ---------------------------------------------- #
+
+# def txt_parse(txt):
+#     with open(txt) as f:
+#         data = f.readlines()
+#         return [l.strip() for l in data]
+
+# with open("test_data.txt") as f:
+#     data_arr = [l.strip().split(' ') for l in f.readlines()]
